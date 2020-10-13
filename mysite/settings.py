793bb105474f41d7f757b04cf33d62b9fb4e623e
NONE-31D <castrod152@swu.ac.kr>
2020-10-12 19:52:57 -0700

    [Refactor Codes] Static route

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,5 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
